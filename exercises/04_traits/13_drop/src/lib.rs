20ed320d6b7b05ad3f70edbada6553c062833621
Thask Leve <thask.leve@gmail.com>
2025-03-23 18:52:11 +0100

    solve 04_traits/13_drop

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -1,6 +1,24 @@
-// TODO: implement a so-called "Drop bomb": a type that panics when dropped
-//  unless a certain operation has been performed on it.
-//  You can see the expected API in the tests below.
+struct DropBomb {
+    active: bool
+}
+
+impl DropBomb {
+    fn new() -> Self {
+        Self { active: true }
+    }
+
+    fn defuse(&mut self) {
+        self.active = false;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if (self.active) {
+            panic!("BOOM")
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
