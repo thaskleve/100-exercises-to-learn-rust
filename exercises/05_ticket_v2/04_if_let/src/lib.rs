92aafe121b9dda648d099a03591877a4ab916821
Thask Leve <thask.leve@gmail.com>
2025-03-25 19:36:21 +0100

    solve 05_ticket_v2/04_if_let

--- exercises/05_ticket_v2/04_if_let/src/lib.rs
+++ exercises/05_ticket_v2/04_if_let/src/lib.rs
@@ -1,3 +1,5 @@
+use core::panic;
+
 enum Shape {
     Circle { radius: f64 },
     Square { border: f64 },
@@ -5,10 +7,12 @@ enum Shape {
 }
 
 impl Shape {
-    // TODO: Implement the `radius` method using
-    //  either an `if let` or a `let/else`.
     pub fn radius(&self) -> f64 {
-        todo!()
+        if let Shape::Circle { radius } = &self {
+            *radius
+        } else {
+            panic!("Only the `Circle` shape has a radius")
+        }
     }
 }
 
