5f2a06bbbe1ad22841820b6ea6f562fbba8745f0
Thask Leve <thask.leve@gmail.com>
2025-03-25 19:38:05 +0100

    solve 05_ticket_v2/05_nullability

--- exercises/05_ticket_v2/05_nullability/src/lib.rs
+++ exercises/05_ticket_v2/05_nullability/src/lib.rs
@@ -35,8 +35,13 @@ impl Ticket {
             status,
         }
     }
+
     pub fn assigned_to(&self) -> Option<&String> {
-        todo!()
+        if let Status::InProgress { assigned_to } = &self.status {
+            Some(assigned_to)
+        } else {
+            None
+        }
     }
 }
 
