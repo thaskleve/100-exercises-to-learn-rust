8fa4da36af575877997a502a23538a762169c8e8
Thask Leve <thask.leve@gmail.com>
2025-03-25 19:36:12 +0100

    solve 05_ticket_v2/03_variants_with_data

--- exercises/05_ticket_v2/03_variants_with_data/src/lib.rs
+++ exercises/05_ticket_v2/03_variants_with_data/src/lib.rs
@@ -37,8 +37,12 @@ impl Ticket {
             status,
         }
     }
+
     pub fn assigned_to(&self) -> &str {
-        todo!()
+        match &self.status {
+            Status::InProgress { assigned_to } => &assigned_to,
+            _ => panic!("Only `In-Progress` tickets can be assigned to someone")
+        }
     }
 }
 
