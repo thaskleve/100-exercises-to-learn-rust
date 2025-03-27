a4e4778b3f8aaabb22edcf55e9f61f0cd0fdb809
Thask Leve <thask.leve@gmail.com>
2025-03-27 09:09:26 +0100

    solve 06_ticket_management/11_mutable_slices

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,5 +1,8 @@
-// TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
-//  The slice should be modified in place.
+pub fn squared(arr: &mut [i32]) {
+    for x in arr.iter_mut() {
+        *x = *x * *x
+    }
+}
 
 #[cfg(test)]
 mod tests {
